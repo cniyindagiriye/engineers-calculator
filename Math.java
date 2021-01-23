f246fb0efd96968555951757c75edec9cef6f6a1
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2021-01-23 15:13:48 +0200

    create add function

---
+++ Math.java
@@ -0,0 +1,11 @@
+class Math {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Math");
+    
+    System.out.println(add(2, 4));
+  }
+}
