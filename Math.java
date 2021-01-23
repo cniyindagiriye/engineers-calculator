80a27bad12b791569de41165d3223da7dd717747
THERESE07 <71461834+THERESE07@users.noreply.github.com>
2021-01-23 15:40:28 +0200

    Substraction function

--- Math.java
+++ Math.java
@@ -2,10 +2,14 @@ class Math {
   public static int add(int a, int b) {
     return a + b;
   }
-
+  
+  public static int sub(int i, int j){
+  	return i-j;
+  }
   public static void main(String[] args) {
     System.out.println("Math");
     
     System.out.println(add(2, 4));
+    System.out.println(sub(4,2));
   }
 }
